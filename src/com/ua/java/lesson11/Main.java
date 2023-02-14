dfd918db27d752e2789c712ba36626b3e741f992
Max White <maxwhit1983@gmail.com>
2023-02-14 14:32:36 +0200

    task11 finished

---
+++ src/com/ua/java/lesson11/Main.java
@@ -0,0 +1,13 @@
+package com.ua.java.lesson11;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Truck truck = new Truck();
+        System.out.println(truck.getCarType());
+
+        Pickup pickup = new Pickup();
+        System.out.println(pickup.getCarType());
+
+    }
+}
